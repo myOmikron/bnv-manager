f3ccdc087010defa0f3ec5b0451a885cc271665a
myOmikron <accounts@omikron.dev>
2021-02-10 22:12:02 +0100

    Added view to add users

--- bnv_verwaltung/management/urls.py
+++ bnv_verwaltung/management/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from management.views import IndexView
+from management.views import IndexView, AddView
 
 urlpatterns = [
     path('index', IndexView.as_view()),
+    path('add', AddView.as_view()),
 ]
--- bnv_verwaltung/management/views.py
+++ bnv_verwaltung/management/views.py
@@ -13,5 +13,32 @@ class IndexView(LoginRequiredMixin, TemplateView):
         l.bind_s(settings.AUTH_LDAP_BIND_DN, settings.AUTH_LDAP_BIND_PASSWORD)
         users = l.search_s(settings.LDAP_USER_DN, ldap.SCOPE_SUBTREE, settings.LDAP_USER_FILTER)
         l.unbind_s()
-        print(users)
         return render(request, self.template_name, {"users": users})
+
+
+class AddView(LoginRequiredMixin, TemplateView):
+    template_name = "management/add.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        from bnv_verwaltung import settings
+        import ldap.modlist
+        l = ldap.initialize(settings.AUTH_LDAP_SERVER_URI)
+        l.bind_s(settings.AUTH_LDAP_BIND_DN, settings.AUTH_LDAP_BIND_PASSWORD)
+        modlist = ldap.modlist.addModlist(
+            {
+                "givenName": [bytes(request.POST["givenName"])],
+                "sn": [bytes(request.POST["sn"])],
+                "mail": [bytes(request.POST["mail"])],
+                "MailQuota": [bytes(request.POST["quota"])],
+                "objectClass": [bytes("BNVuser"), bytes("top")],
+                "uid": [bytes(request.POST["uid"])],
+                "Verein": [bytes(request.POST["verein"])],
+
+            }
+        )
+        l.add_s(request.POST["uid"] + settings.LDAP_USER_DN, modlist=modlist)
+        l.unbind_s()
+        return render(request, self.template_name)
