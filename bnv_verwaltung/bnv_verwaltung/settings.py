cbcde86a7f35f039c15b83da7139ecbbdb221081
myOmikron <accounts@omikron.dev>
2021-02-10 20:27:49 +0100

    Set template dir and added installed apps

--- bnv_verwaltung/bnv_verwaltung/settings.py
+++ bnv_verwaltung/bnv_verwaltung/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'administration',
+    'generic',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,9 @@ ROOT_URLCONF = 'bnv_verwaltung.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
