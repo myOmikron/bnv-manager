e7638407fd73b67c3ae5f53630b427cd85529d4b
myOmikron <accounts@omikron.dev>
2021-02-11 14:01:49 +0100

    Fixed missing paratheses

--- bnv_verwaltung/management/urls.py
+++ bnv_verwaltung/management/urls.py
@@ -5,5 +5,5 @@ from management.views import IndexView, AddView, DeleteView
 urlpatterns = [
     path('index', IndexView.as_view()),
     path('add', AddView.as_view()),
-    path('delete/<str:k>', DeleteView.as_view),
+    path('delete/<str:k>', DeleteView.as_view()),
 ]
