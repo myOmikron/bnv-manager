5daaaa77dbf5e43d0e9489e38ddeff8529030fbc
myOmikron <accounts@omikron.dev>
2021-02-11 13:40:43 +0100

    Set attributes to receive

--- bnv_verwaltung/management/views.py
+++ bnv_verwaltung/management/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from bnv_verwaltung import settings
+
 
 def make_secret(password):
     salt = os.urandom(4)
@@ -20,10 +22,12 @@ class IndexView(LoginRequiredMixin, TemplateView):
     template_name = "management/index.html"
 
     def get(self, request, *args, **kwargs):
-        from bnv_verwaltung import settings
         l = ldap.initialize(settings.AUTH_LDAP_SERVER_URI)
         l.bind_s(settings.AUTH_LDAP_BIND_DN, settings.AUTH_LDAP_BIND_PASSWORD)
-        users = l.search_s(settings.LDAP_USER_DN, ldap.SCOPE_SUBTREE, settings.LDAP_USER_FILTER)
+        users = l.search_s(settings.LDAP_USER_DN,
+                           ldap.SCOPE_SUBTREE,
+                           settings.LDAP_USER_FILTER,
+                           attrlist=["givenName", "sn", "mail", "MailQuota", "Verein", "uid"])
         l.unbind_s()
         return render(request, self.template_name, {"users": users})
 
