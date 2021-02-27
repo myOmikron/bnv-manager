728ae4a625bdf2c8d1207cc8c7d4da553d309a80
myOmikron <accounts@omikron.dev>
2021-02-27 22:57:05 +0100

    Added static dir

--- bnv_manager/bnv_manager/settings.py
+++ bnv_manager/bnv_manager/settings.py
@@ -130,6 +130,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
