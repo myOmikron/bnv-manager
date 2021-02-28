13d49affdea9eb9e0bfcb87fea980b68c39a090d
myOmikron <accounts@omikron.dev>
2021-02-28 15:29:48 +0100

    Added domain and advancedGroups as models

--- bnv_manager/generic/models.py
+++ bnv_manager/generic/models.py
@@ -1,7 +1,16 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.db import models
-from django.db.models import OneToOneField
+from django.db.models import OneToOneField, ManyToManyField, CharField
+
+
+class Domain(models.Model):
+    name = CharField(max_length=255, default="")
 
 
 class AdvancedUser(models.Model):
     user = OneToOneField(User, on_delete=models.CASCADE)
+
+
+class AdvancedGroup(models.Model):
+    model = OneToOneField(Group, on_delete=models.CASCADE)
+    associated_domains = ManyToManyField(Domain)
