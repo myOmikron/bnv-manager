8512c7c5b867642be47d53562b1a042de80c81cc
myOmikron <accounts@omikron.dev>
2021-02-28 03:46:44 +0100

    Added views for admins

--- bnv_manager/administration/urls.py
+++ bnv_manager/administration/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from administration.views import IndexView
+from administration.views import IndexView, DomainView, GroupView, GroupAddView, GroupDeleteView
 
 urlpatterns = [
-    path("index", IndexView.as_view())
+    path("index", IndexView.as_view()),
+    path("domains", DomainView.as_view()),
+    path("groups", GroupView.as_view()),
+    path("groups/add", GroupAddView.as_view()),
+    path("groups/delete/<str:name>", GroupDeleteView.as_view()),
 ]
--- bnv_manager/administration/views.py
+++ bnv_manager/administration/views.py
@@ -1,10 +1,72 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.contrib.auth.models import Group
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+
+from generic.mailcow import get_domains
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = "administration/index.html"
 
     def get(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return render(request, "generic/notallowed.html")
         return render(request, self.template_name)
+
+
+class DomainView(LoginRequiredMixin, TemplateView):
+    template_name = "administration/domain.html"
+
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return render(request, "generic/notallowed.html")
+        domain_list = get_domains()
+        group_list = Group.objects.filter(name__regex=r"^(?!.*superuser).*$")
+        return render(
+            request,
+            self.template_name,
+            {
+                "domain_list": domain_list,
+                "group_list": group_list
+            }
+        )
+
+
+class GroupView(LoginRequiredMixin, TemplateView):
+    template_name = "administration/groups.html"
+
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return render(request, "generic/notallowed.html")
+        group_list = Group.objects.filter(name__regex=r"^(?!.*superuser).*$")
+        return render(request, self.template_name, {"group_list": group_list})
+
+
+class GroupAddView(LoginRequiredMixin, TemplateView):
+    template_name = "generic/notallowed.html"
+
+    def post(self, request):
+        name = request.POST["name"]
+        if not request.user.is_superuser:
+            return render(request, "generic/notallowed.html")
+        group, created = Group.objects.get_or_create(name=name)
+        if not created:
+            return render(request, "generic/info.html", {"message": "This group already exists."})
+        group.save()
+        return redirect("/administration/groups")
+
+
+class GroupDeleteView(LoginRequiredMixin, TemplateView):
+    template_name = "generic/notallowed.html"
+
+    def post(self, request, name="", *args, **kwargs):
+        print(request.user)
+        if not request.user.is_superuser:
+            return render(request, "generic/notallowed.html")
+        try:
+            group = Group.objects.get(name=name)
+            group.delete()
+        except Group.DoesNotExist:
+            return render(request, "generic/info.html", {"message": "This group does not exist."})
+        return redirect("/administration/groups")
