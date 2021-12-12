27cf7379093e46ea4183f13e74deea2df7b8bd8f
myOmikron <accounts@omikron.dev>
2021-12-12 02:41:42 +0100

    Added abbreviation to Group

--- bnv_manager/generic/models.py
+++ bnv_manager/generic/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 from django.db import models
 
 
 class Club(models.Model):
+    abbreviation = models.CharField(default="", max_length=16, validators=[RegexValidator(regex=r"^[a-zA-Z0-9]+$")])
     name = models.CharField(default="", max_length=1024)
     associated_managers = models.ManyToManyField(User, blank=True)
