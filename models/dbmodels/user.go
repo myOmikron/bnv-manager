7db102517f0a589522486141c6545d2d75513aa3
myOmikron <git@omikron.dev>
2022-10-18 02:51:01 +0200

    Added last login field

--- models/dbmodels/user.go
+++ models/dbmodels/user.go
@@ -13,12 +13,16 @@ type User struct {
 	DN          string `gorm:"unique"`
 	IsClubAdmin bool   `gorm:"not null"`
 	IsAdmin     bool   `gorm:"not null"`
+	LastLogin   *time.Time
 }
 
 func (u *User) GetAuthModelIdentifier() (string, uint) {
 	return "bnv_ldap", u.ID
 }
 
-func (u *User) UpdateLastLogin(echo.Context, *gorm.DB, time.Time) {
-
+func (u *User) UpdateLastLogin(c echo.Context, db *gorm.DB, login time.Time) {
+	u.LastLogin = &login
+	if err := db.Save(&u).Error; err != nil {
+		c.Logger().Error(err)
+	}
 }
