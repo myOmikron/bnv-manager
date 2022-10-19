667d189910cb593d8e3e84d0c39e491bb842bba5
myOmikron <git@omikron.dev>
2022-10-19 02:51:53 +0200

    Return ok if no cookie is present

--- handler/logout.go
+++ handler/logout.go
@@ -1,12 +1,16 @@
 package handler
 
 import (
+	"errors"
 	"github.com/labstack/echo/v4"
 	"github.com/myOmikron/echotools/middleware"
 )
 
 func (w *Wrapper) Logout(c echo.Context) error {
 	if err := middleware.Logout(w.DB, c); err != nil {
+		if errors.Is(middleware.ErrCookieNotFound, err) {
+			return c.NoContent(200)
+		}
 		c.Logger().Error(err)
 		return c.String(500, "Internal server error")
 	}
