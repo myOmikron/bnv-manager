54fb64e35502d225aa29a0b3f2d1cdf3cb9f3334
Chris <chris@hopfen.space>
2022-10-19 02:46:54 +0200

    Added a draft club admin page

---
+++ frontend/src/lib/admin.ts
@@ -0,0 +1,33 @@
+interface ClubAdminResponse {
+    username: null | string,
+    error: null | string,
+    success: boolean
+}
+
+export async function createClubAdmin(firstname: string, surname: string, password: string, club_id: string): Promise<ClubAdminResponse> {
+    let response = await fetch("/api/clubadmins", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            firstname: firstname,
+            surname: surname,
+            password: password,
+            club_id: club_id
+        })
+    });
+    if (!response.ok)
+        return {
+            username: null,
+            error: await response.text(),
+            success: false
+        };
+
+    let json = await response.json();
+    return {
+        username: json.username,
+        error: null,
+        success: true
+    }
+}
---
+++ frontend/src/routes/app/admin/+page.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+
+    import { createClubAdmin } from '../../../lib/admin';
+
+    async function handleGeneratePasswordButton(event) {
+        // TODO
+        console.log("generate password button");
+        alert("not implemented yet");
+    }
+
+    async function handleCreateClubAdmin(event: SubmitEvent) {
+        // TODO
+        console.log("handle create club admin submit")
+    }
+</script>
+
+<div id="page">
+    <h2>Admin</h2>
+
+    <div class="clubadmins">
+        Coming soon: List of all known clubadmins
+        <form method="POST" on:submit|preventDefault={handleCreateClubAdmin}>
+            <h3>Add a new club admin</h3>
+            <label>
+                <span>Vorname</span>
+                <input name="firstname" type="text" />
+            </label>
+            <label>
+                <span>Nachname</span>
+                <input name="surname" type="text" />
+            </label>
+            <label>
+                <span>Vereins-ID</span>
+                <input name="surname" type="text" />
+            </label>
+            <br/>
+            <label>
+                <span>Passwort</span>
+                <input name="password" type="text" />
+                <input on:click={handleGeneratePasswordButton} name="generate" type="button" value="Generieren" />
+            </label>
+            <br/>
+            <input type="submit" value="Anlegen" />
+        </form>
+    </div>
+</div>
+
+<style>
+    /* Temporary */
+    .clubadmins {
+        border: 1px solid black;
+    }
+
+    /* Temporary */
+    .clubadmins form {
+        border: 1px solid grey;
+    }
+</style>
